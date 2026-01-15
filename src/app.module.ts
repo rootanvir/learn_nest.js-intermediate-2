a7fecfc97be94dca5ed91d892ff1c03c8b7aab1c
rootanvir <tanvir.22464751@gmail.com>
2026-01-16 01:58:34 +0600

    31/104 (db connection)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [UsersModule, TweetModule, AuthModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    entities: [],
+    synchronize: true,
+    host: 'localhost',
+    username: 'postgres',
+    password: '1234',
+    database: 'nestjs',
+
+  })],
   controllers: [AppController, AuthController],
   providers: [AppService],
 })
